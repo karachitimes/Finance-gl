0ac0f2de226645af946f5b74ef5529142295ba38
Naushad Anwar <mrnaushadanwar@gmail.com>
2026-01-31 13:06:58 +0500

    Delete embedding_engine.py

--- embedding_engine.py
+++
@@ -1,8 +0,0 @@
-
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed_text(texts):
-    return model.encode(texts, normalize_embeddings=True)
