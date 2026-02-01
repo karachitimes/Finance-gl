3ba285250812d137a3e76e42d968ad55d22a57e8
Naushad Anwar <mrnaushadanwar@gmail.com>
2026-02-01 14:47:04 +0500

    Update parameters in build_where_from_ui function

--- dashboards.py
+++ dashboards.py
@@ -85,7 +85,7 @@ def render_cashflow_tab(engine, f):
     rel_cf = pick_view(engine, REL_CF, rel_sem)
 
     where, params, _ = build_where_from_ui(
-        f["df"], f["dt"], f["bank"], f["head"], f["account"], f["attribute"], f["func_code"],
+        f["df"], f["dt"], f["bank"], f["head"], f["head_no"], f["account"], f["attribute"], f["func_code"],
         fy_label=f["fy_label"], func_override=None
     )
     where_sql = " and ".join(where) if where else "1=1"
